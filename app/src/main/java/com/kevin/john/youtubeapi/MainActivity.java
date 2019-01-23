70767c4c23f96d0b1a9923630526656036358fa3
MrJKei <johnkevinleonardo@gmail.com>
2019-01-23 15:26:11 +0800

    Initial commit

---
+++ app/src/main/java/com/kevin/john/youtubeapi/MainActivity.java
@@ -0,0 +1,47 @@
+package com.kevin.john.youtubeapi;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    Button add;
+    private YouTubePlayerView youTubePlayerView;
+    private YouTubePlayer.OnInitializedListener onInitializedListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        youTubePlayerView = findViewById(R.id.youtube_view);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+       add = findViewById(R.id.play);
+       add.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               youTubePlayerView.initialize("",onInitializedListener);
+           }
+       });
+
+    }
+}
